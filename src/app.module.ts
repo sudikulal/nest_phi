66d748a8ab5ccf6069c5b55a9da17f9760f080da
sudikulal <sudikulal325@gmail.com>
2025-05-30 10:35:48 +0530

    added config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import config from './global/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserModule } from './user/user.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
